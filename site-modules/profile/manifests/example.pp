fe85286a32bbd2767b536177d395389aae23b95d
bayaprakash <cbaya.reddy8@gmail.com>
2023-07-16 23:08:31 +0530

    Update example.pp

--- site-modules/profile/manifests/example.pp
+++ site-modules/profile/manifests/example.pp
@@ -1,6 +1,15 @@
 # An example profile
 class profile::example {
+"windows":
+{
 class { 'puppet_agent':
     package_version => '7.23.0',
    }
+}
+"Linux":
+{
+class { 'puppet_agent':
+    package_version => '7.23.0',
+   }
+}
 }
